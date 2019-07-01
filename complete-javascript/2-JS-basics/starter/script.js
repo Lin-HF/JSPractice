1b1bc3ff261c0fba67cb3da9822741be89c48cae
David Lin <haiflin@iu.edu>
2019-07-01 11:34:18 -0400

    variable mutation

--- complete-javascript/2-JS-basics/starter/script.js
+++ complete-javascript/2-JS-basics/starter/script.js
@@ -33,3 +33,11 @@ var job, isMarried;
 job = 'Teacher';
 isMarried = false;
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Vairable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + '' + lastName);
