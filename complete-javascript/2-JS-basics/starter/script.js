76dcc95d91e5843b332d25e08598543ccfe03e47
David Lin <haiflin@iu.edu>
2019-07-01 11:30:11 -0400

    type coercion

---
+++ complete-javascript/2-JS-basics/starter/script.js
@@ -0,0 +1,35 @@
+/*
+                varibales and data types
+*/
+
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullage = true;
+// console.log(fullage);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+// var _3year = 3;
+// var johnMark = 'John and Mark';
+
+/*
+                Variable mutation and type coercion
+*/
+
+//Type coercion
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
